169e8c002929ffa484ab3d0d7d66e2234397de33
Luis Sarmiento <48292291+dudunesto@users.noreply.github.com>
2026-02-25 19:38:56 +0000

    Listado Luis Sarmiento

--- src/js/components/ToDo.jsx
+++ src/js/components/ToDo.jsx
@@ -7,23 +7,45 @@ const ToDo = () => {
     function agregarItem() {
         if (input.trim() === "") return;
 
-        setList([...list, input]);
+        setList([...list, { id: Date.now(), text: input }]);
         setInput("");
     }
 
-    const agregarItems = (e) => {
-        setInput(e.target.value);
-    };
+    function eliminarItem(id) {
+        setList(list.filter(item => item.id !== id));
+    }
 
     return (
         <div className="container mx-auto mt-5">
-            <input className="form-control" type="text" value={input} onChange={agregarItems}/>
+            <input
+                className="form-control"
+                type="text"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+            />
 
-            <ul className="list-group">
-                {list.map((item, index) => (
-                    <li className="list-group-item" key={index}>{item}</li>
+            <button
+                className="btn btn-primary mt-2"
+                onClick={agregarItem}
+            >
+                Agregar
+            </button>
+
+            <ul className="list-group mt-3">
+                {list.map((item) => (
+                    <li
+                        key={item.id}
+                        className="list-group-item d-flex justify-content-between align-items-center"
+                    >
+                        {item.text}
+                        <button
+                            className="btn btn-danger btn-sm"
+                            onClick={() => eliminarItem(item.id)}
+                        >
+                            X
+                        </button>
+                    </li>
                 ))}
-                <button className="btn btn-primary" onClick={agregarItem}>Agregar</button>
             </ul>
         </div>
     );
